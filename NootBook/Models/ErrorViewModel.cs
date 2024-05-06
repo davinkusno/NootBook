fb82c817c0fe429425d682a52e687a1a858e7425
.Ganku <mister.gentleguy@gmail.com>
2024-05-06 21:59:00 +0700

    Add project files.

---
+++ NootBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NootBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
