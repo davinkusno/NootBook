c3f83a32203b19d22b5764e536b85322b2cf9cc4
.Ganku <mister.gentleguy@gmail.com>
2024-05-09 21:39:43 +0700

    Added the .NET Framework 4.8.1 version of the project.

---
+++ NootBook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NootBook.MvcApplication" Language="C#" %>
--- NootBook/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NootBook.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
